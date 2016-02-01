836df725878f827636aadd07ca1176bd50f36200
Tydax <tydaxx@gmail.com>
2016-02-01 18:07:48 +0100

    Added work from practical

--- tp01/ex_2.sql
+++ tp01/ex_2.sql
@@ -6,3 +6,41 @@
  * Exercice 2
  *
  */
+
+-- Question 1
+SELECT  annee,
+        cl_r,
+        category,
+        AVG(qte * pu) AS ca_moyen
+FROM    ventes v NATURAL JOIN temps
+                 NATURAL JOIN produits
+                 INNER JOIN clients c ON (c.cl_id = v.cid)
+WHERE   annee IN(2009, 2010)
+GROUP BY ROLLUP (annee, cl_r, category);
+
+-- Question 2
+SELECT  annee,
+        cl_r,
+        category,
+        AVG(qte * pu) AS ca_moyen
+FROM    ventes v NATURAL JOIN temps
+                 NATURAL JOIN produits
+                 INNER JOIN clients c ON (c.cl_id = v.cid)
+WHERE   annee IN(2009, 2010)
+GROUP BY CUBE(annee, cl_r, category);
+
+-- Question 3 TO FINISH
+SELECT  RANK() OVER (PARTITION BY annee, category ORDER BY nb DESC) AS rang,
+        annee,
+        category,
+        pname, nb
+FROM (
+    SELECT SUM(qte) AS nb,
+            annee,
+            category,
+            pname
+    FROM   ventes v NATURAL JOIN temps
+                    NATURAL JOIN produits
+                    INNER JOIN clients c ON (c.cl_id = v.cid)
+    GROUP BY annee, category, pname
+) q;
