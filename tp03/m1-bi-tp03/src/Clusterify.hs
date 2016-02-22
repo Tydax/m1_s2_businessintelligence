23f867b18ae1478160550c138208559b052a438b
Tydax <tydaxx@gmail.com>
2016-02-22 17:20:12 +0100

    Added Haskell types

---
+++ tp03/m1-bi-tp03/src/Clusterify.hs
@@ -0,0 +1,13 @@
+{- |
+  Module      :  $Header$
+  Description :  
+  Copyright   :  None
+  License     :  None
+  Maintainer  :  tydax@protonmail.ch
+  Stability   :  unstable
+  The $Header$ module describes all the types used in the application. Many of the types
+  are here just to make the code easier to understand and read.
+-}
+module Clusterify (
+  
+) where
---
+++ tp03/m1-bi-tp03/src/Types.hs
@@ -0,0 +1,32 @@
+{- |
+  Module      :  $Header$
+  Description :  Different types used in algorithms
+  Copyright   :  None
+  License     :  None
+  Maintainer  :  tydax@protonmail.ch
+  Stability   :  unstable
+  The $Header$ module describes all the types used in the algorithms. Many of the types
+  are here just to make the code easier to understand and read.
+-}
+module Types (
+  Centre,
+  Cluster,
+  Data,
+  Distance
+) where
+
+-- |The 'Cluster' type describes a cluster of data which distance is short.
+data Cluster = Cluster [Data] Centre
+deriving (Show)
+
+-- |The 'Centre' type is an alias to describe the centre of a cluster.
+type Centre = Data
+
+-- |The 'Data' type describes a type used to represent the data to clusterify.
+data Data = Data {
+    values :: [Double]
+  , distanceCentre :: Double
+  } deriving (Show)
+
+-- |The 'Distance' type describes a function taking two sets of values and computing a distance.
+type Distance = Data -> Data -> Double
